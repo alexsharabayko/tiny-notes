85bf1761bae60a044604f023ecb8969a96da3e3b
Alex Sharabayko <Alexander.Sharabayko@idt.net>
2021-10-03 23:56:32 +0200

    chore: [UI-0] remove not working tests

--- src/__tests__/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Todo from '../components/Todo';
-
-test('renders without throwing errors', () => {
-  const container = render(<Todo />);
-  expect(container).toMatchSnapshot();
-});
